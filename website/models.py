faa88b4c38b5bd1f5338aafdcc543513643c109c
Markus Rauhalahti <mrauhalahti@gmail.com>
2018-04-27 08:43:49 +0300

    created a model for articles

--- website/models.py
+++ website/models.py
@@ -19,3 +19,18 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+class Article(models.Model):
+    doi = models.CharField(max_length=200) # doi, url is created automatically from this
+    url = "http://doi.org/{}".format(doi)
+    authors = models.CharField(max_length=1000)
+    title = models.CharField(max_length=1000)
+    journal = models.CharField(max_length=200)
+    pages_and_volume = models.CharField(max_length=200)
+    year = models.DateField(
+            default=timezone.now)
+    description = models.TextField() # Abstract eg
+    #image = models.ImageField(upload_to="article_images/") 
+    status = model.CharField(max_length=300) # Submitted, being reviewed, published
+    def __str__(self):
+        return self.title
